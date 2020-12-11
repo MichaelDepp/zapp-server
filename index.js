14089f53ebfb5970cda5995d981d78e15634ee68
MichaelDepp <michaeldepp7@gmail.com>
2020-12-11 11:40:55 +0800

    user register done

--- index.js
+++ index.js
@@ -175,6 +175,74 @@ app.post("/emailidcheck", (req, res) => {
   }
 });
 
+app.post("/registeruser", (req, res) => {
+  console.log(req.body.data);
+
+  const welcomeEmail = {
+    currentLocation: "inbox",
+    id: "email1",
+    initialLocation: "inbox",
+    message:
+      "Welcome to Zapp Email Service. We are the world first email service specially tailored for blind people.",
+    read: false,
+    receiverEmail: req.body.data.email,
+    receiverName: req.body.data.fname + " " + req.body.data.lname,
+    senderEmail: "team@zapp.com",
+    senderName: "Zapp Official",
+    timestamp: "11-12-2020",
+    title: "Welcome to Zapp " + req.body.data.fname,
+  };
+
+  const alert1 = {
+    from: "Zapp Team",
+    message: "1 message unread",
+    read: false,
+    timestamp: "11-12-2020",
+  };
+
+  let register = new Promise((resolve, reject) => {
+    try {
+      db.ref("accounts/" + ("registry" + req.body.data.id)).set({
+        email: req.body.data.email,
+        password: req.body.data.password,
+        userid: req.body.data.id,
+      });
+      resolve("register");
+    } catch (e) {
+      reject("not register");
+    }
+  });
+
+  register
+    .then((message) => {
+      if (message === "register") {
+        try {
+          db.ref("users/" + req.body.data.id).set({
+            emails: {
+              email1: welcomeEmail,
+            },
+            notification: {
+              alert1: alert1,
+            },
+            userinfo: {
+              age: req.body.data.age,
+              fname: req.body.data.fname,
+              lname: req.body.data.lname,
+              location: req.body.data.city + ", " + req.body.data.country,
+              phone: req.body.data.phone,
+            },
+          });
+          res.json({ message: "complete" });
+        } catch (e) {
+          res.json({ message: "failed" });
+        }
+      }
+    })
+    .catch((message) => {
+      res.json({ message });
+    });
+});
+
 app.listen(port, () => {
   console.log(`Zapp server is listening at http://localhost:${port}`);
 });
